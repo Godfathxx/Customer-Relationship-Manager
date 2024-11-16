8e29c2ed2c2bb878a26981abbf724acd37336ea5
Shreyash Joshi <90604516+Godfathxx@users.noreply.github.com>
2024-11-16 20:51:39 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000', // Allow only your frontend's origin
+  origin: 'https://customer-relationship-manager-frontend-t1aa.vercel.app/', // Allow only your frontend's origin
   credentials: true, // Enable credentials for session management
 }));
 app.use(express.json());
